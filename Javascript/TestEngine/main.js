68a4ab66d8f0ca12adc736620ce04e69bb653508
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2020-02-21 12:31:16 +0530

    Add files via upload

--- Javascript/TestEngine/main.js
+++ Javascript/TestEngine/main.js
@@ -1,7 +1,34 @@
 window.addEventListener("load",initEvent);
 
 function initEvent() {
+    document.querySelector("#ins_ques").addEventListener("click",insertQues);
+    document.querySelector("#del_ques").addEventListener("click",deleteQues);
+    document.querySelector("#saveTest").addEventListener("click",saveTest);
     loadStudents();
+    loadTest();
+}
+
+function saveTest() {
+    if(window.localStorage) {
+        var data = JSON.stringify(obj.quesList);
+        localStorage.setItem("quesList",data);
+    }
+    else {
+        alert("Localstorage not supported...");
+    }
+}
+
+function loadTest() {
+    if(window.localStorage) {
+        if(localStorage.quesList) {
+            var ques = localStorage.getItem('quesList');
+            obj.quesList = JSON.parse(ques);
+            showQuestions();
+        }
+    }
+    else {
+        alert("Localstorage not supported...");
+    }
 }
 
 function loadStudents() {
@@ -23,4 +50,47 @@ function loadStudents() {
         tr.insertCell().innerHTML = obj.phone;
         table.appendChild(tr);
      });
+}
+
+function insertQues() {
+    var subject = document.querySelector("#subject").value;
+    var ques = document.querySelector("#ques").value;
+    var opt1 = document.querySelector("#opt_1").value;
+    var opt2 = document.querySelector("#opt_2").value;
+    var opt3 = document.querySelector("#opt_3").value;
+    var opt4 = document.querySelector("#opt_4").value;
+    var ans = document.querySelector("#ans").value;
+    obj.add_ques(subject,ques,opt1,opt2,opt3,opt4,ans);
+
+    showQuestions();
+}
+
+function showQuestions() {
+    var table = document.querySelector("#quesList");
+    table.innerHTML = "";
+    obj.quesList.forEach(function (obj) { 
+        var tr = table.insertRow();
+        tr.insertCell().innerHTML = obj.q_id;
+        tr.insertCell().innerHTML = obj.subject;
+        tr.insertCell().innerHTML = obj.ques;
+        tr.insertCell().innerHTML = obj.opt1;
+        tr.insertCell().innerHTML = obj.opt2;
+        tr.insertCell().innerHTML = obj.opt3;
+        tr.insertCell().innerHTML = obj.opt4;
+        tr.insertCell().innerHTML = obj.ans;
+        table.appendChild(tr);
+        tr.addEventListener("click",selectRow);
+     });
+}
+
+function selectRow() {
+    event.srcElement.parentElement.classList.toggle('bg-danger');
+    var qid = event.srcElement.parentElement.children[0].innerHTML;
+    // console.log(parseInt(qid));
+    obj.select_ques(parseInt(qid))
+}
+
+function deleteQues() {
+    obj.delete_ques();
+    showQuestions();
 }
